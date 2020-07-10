23445fb797314d736a323fe005ebd88ca25ee758
guivicentep <guivicente.gvp@hotmail.com>
2020-07-10 14:19:44 -0300

    Created Register Screen

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,13 @@
 import React, {useState} from 'react';
 import { ScrollView, View, Text, TouchableOpacity, Picker } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { AntDesign } from '@expo/vector-icons'; 
+import { AntDesign, FontAwesome5, Ionicons } from '@expo/vector-icons'; 
 import { Rajdhani_400Regular, useFonts } from '@expo-google-fonts/rajdhani';
 import { AppLoading } from 'expo';
 
 import styles from './styles';
 
 import { TextInput } from 'react-native-gesture-handler';
-import { onChange } from 'react-native-reanimated';
 
 export default function Login() {
     let [fontsLoaded] = useFonts({
@@ -16,16 +15,10 @@ export default function Login() {
       });
 
     const navigation = useNavigation();
-    const [date, setDate] = useState(new Date(1598051730000));
-    const [mode, setMode] = useState('date');
-    const [show, setShow] = useState(false);
+
 
     function navigateToLogin() {
         navigation.navigate('Login');
-    }
-
-    function navigateToNextStep() {
-        navigation.navigate('ForgotPassword2');
     }
 
     if (!fontsLoaded){
@@ -35,17 +28,28 @@ export default function Login() {
     return (
             <View style={styles.container}>
                 <View style={styles.body}>
-                <TouchableOpacity style={styles.btnBack} onPress={navigateToLogin}>
-                    <AntDesign name="left" size={33} color="white" />
-                    <Text style={styles.txtBack}>Login</Text>
-                </TouchableOpacity>
-                    <Text style={styles.txtHelp}>
-                       Vamos começar?</Text>
-                    <TextInput style={styles.txtInputEmail} placeholder="E-mail" />
-                    <TextInput style={styles.txtInputEmail} placeholder="Gênero" />
-                    <TextInput style={styles.txtInputEmail} placeholder="        /         /  " />
-                    <TextInput style={styles.txtInputEmail} placeholder="E-mail" />
-                    <TextInput style={styles.txtInputEmail} placeholder="E-mail" />
+                    <TouchableOpacity style={styles.btnBack} onPress={navigateToLogin}>
+                        <AntDesign name="left" size={33} color="white" />
+                        <Text style={styles.txtBack}>Login</Text>
+                    </TouchableOpacity>
+                        <Text style={styles.txtHelp}>
+                        Vamos começar?</Text>
+                        <TextInput style={styles.txtInputAll} placeholder="Nome completo" />
+                        <TextInput style={styles.txtInputAll} placeholder="Gênero" />
+                        <Text style={styles.txtDate}>Data de nascimento</Text>
+                        <View style={styles.containerDate}>
+                        <TextInput style={styles.txtInputDate} placeholder="        /         /  " />
+                        <FontAwesome5 name="calendar-alt" size={35} color="white" style={styles.calendar}/>
+                        </View>
+                        <TextInput style={styles.txtInputAll} placeholder="Eu sou" />
+                        <TextInput style={styles.txtInputAll} placeholder="E-mail" />
+                    <View style={styles.containerCheckBox}>
+                        <Ionicons name="md-checkbox-outline" size={24} color="white" style={styles.checkBox}/>
+                        <Text style={styles.txtCheckBox}>Desejo receber notificações de novas postagens</Text>
+                    </View>
+                        <TouchableOpacity style={styles.btnLogin} onPress={navigateToLogin}>
+                        <Text style={styles.btnLoginText}>Continuar</Text>
+                    </TouchableOpacity>
 
                 </View>
 
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -33,7 +33,7 @@ export default StyleSheet.create({
         color: '#85EAD5',
     },
 
-    txtInputEmail: {
+    txtInputAll: {
         height: 47,
         width: '95%',
         backgroundColor: '#FFFFFF',
@@ -42,6 +42,59 @@ export default StyleSheet.create({
         fontSize: 28,
         paddingHorizontal: 15,
         fontFamily: "Rajdhani_400Regular"
+    },
+    txtInputDate: {
+        height: 47,
+        width: '75%',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 15,
+        marginTop: '3%',
+        fontSize: 28,
+        paddingHorizontal: 10,
+        fontFamily: "Rajdhani_400Regular"
+    },
+    txtDate: {
+        fontFamily: "Rajdhani_400Regular", 
+        color: '#fff',
+        fontSize: 20,
+        marginTop: '5%'
+    },
+    containerDate: {
+        flexDirection: 'row'
+    },
+    calendar: {
+        marginTop: '3%',
+        marginLeft: 'auto',
+        marginRight: '10%'
+
+    },
+    btnLogin: {
+        marginTop: '12%',
+        borderRadius: 20,
+        backgroundColor: '#85EAD5',
+        borderWidth: 0.5,
+        width:'95%',
+        height: 46,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    btnLoginText: {
+        fontFamily: "Rajdhani_400Regular",
+        fontSize: 20,
+        color: '#1D3570'
+    },
+    containerCheckBox: {
+        flexDirection: 'row' 
+    },
+    txtCheckBox: {
+        fontFamily: "Rajdhani_400Regular", 
+        color: '#fff',
+        fontSize: 17,
+        marginTop: '5%'
+    },
+    checkBox: {
+        marginRight: '5%',
+        marginTop: '5%'
     },
 
 
